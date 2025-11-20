7140665b1f7ce700c3f9e4efc2093d0b35b2276b
Fathima-naj <fathimanajamuneer@gmail.com>
2025-11-20 09:17:52 +0530

    bug:budget tracker error fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/LandingPage'
-import Register from './pages/auth/register'
-import Login from './pages/auth/login'
+import Register from './pages/auth/Register'
+import Login from './pages/auth/Login'
 import Dashboard from './pages/Dashboard'
 // import { Toaster } from 'react-hot-toast'
 
